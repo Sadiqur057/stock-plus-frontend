b4b0adbda23bef80740f4e1963db39c04929f398
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-01-12 23:03:22 +0600

    designed product attribute page

---
+++ src/app/(dashboard-layout)/dashboard/products/customize-product/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import CustomizeProduct from "@/components/dashboard/Product/CustomizeProduct";
+const page = () => {
+  return <CustomizeProduct />;
+};
+
+export default page;
