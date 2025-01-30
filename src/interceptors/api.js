3b1c4be539826bc86269f6164c6238e7a53e3350
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-01-30 13:50:58 +0600

    configured axios interceptor

---
+++ src/interceptors/api.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { deleteCookie, getCookie } from "cookies-next";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  const token = getCookie("stock_plus");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      deleteCookie("stock_plus");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
