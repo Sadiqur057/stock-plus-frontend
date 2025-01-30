1a8488d5b54612d1103a9d482ffba4f95f02fcbd
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-01-30 13:51:46 +0600

    added types of user

---
+++ src/types/user.type.ts
@@ -0,0 +1,14 @@
+export interface UserShape {
+  _id: string;
+  email: string;
+  name: string;
+  password: string;
+  created_at: string;
+  role: string;
+  isVerified: boolean;
+  company_description: string;
+  company_name: string;
+  company_type: string;
+  facebook_url: string;
+  twitter_url: string;
+}
