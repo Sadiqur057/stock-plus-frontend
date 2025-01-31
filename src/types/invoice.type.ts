19bdae0d7d010339985524bd35df6bfdf6806dd8
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-01-31 12:37:57 +0600

    added invoices type

---
+++ src/types/invoice.type.ts
@@ -0,0 +1,37 @@
+export type Product = {
+  id: string;
+  name: string;
+  company: string;
+  price: number;
+  quantity: number;
+};
+
+export type Customer = {
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+};
+
+export type CostSummary = {
+  subtotal: number;
+  total: number;
+  tax: number;
+  total_paid: number;
+  total_due: number;
+  status: string;
+};
+
+export type Invoice = {
+  _id: string;
+  company: {
+    name: string;
+    location: string;
+  };
+  customer: Customer;
+  products: Product[];
+  created_by: string;
+  user_email: string;
+  cost_summary: CostSummary;
+  created_at: string;
+};
