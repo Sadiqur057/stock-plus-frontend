c72d8ff1facb99d15236777e2238f0df4d280596
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-01-31 19:19:42 +0600

    handled dashboard layout

--- src/app/(dashboard-layout)/dashboard/page.tsx
+++ src/app/(dashboard-layout)/dashboard/page.tsx
@@ -1,24 +1,6 @@
-import BreadCrumb from "@/components/shared/dashboard/BreadCrumb";
-import { SidebarInset } from "@/components/ui/sidebar";
+import DashboardHome from "@/components/dashboard/Home/DashboardHome";
+
 
 export default function Page() {
-  const breadcrumbList = [
-    {
-      name: "Dashboard",
-      link: '/dashboard'
-    },
-  ]
-  return (
-    <SidebarInset>
-      <BreadCrumb breadcrumbList={breadcrumbList}/>
-      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-          <div className="aspect-video rounded-xl bg-muted/50" />
-        </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-      </div>
-    </SidebarInset>
-  );
+  return <DashboardHome />;
 }
---
+++ src/components/dashboard/Home/DashboardHome.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { RecentSales } from "./RecentSales";
+import { Overview } from "./Overview";
+import { SidebarInset } from "@/components/ui/sidebar";
+import BreadCrumb from "@/components/shared/dashboard/BreadCrumb";
+import { InvoiceStats } from "./InvoiceStats";
+import { PaymentOverview } from "./PaymentOverview";
+import { RecentTransactions } from "./RecentTransaction";
+
+const DashboardHome = () => {
+  const breadcrumbList = [
+    {
+      name: "Home",
+      link: "/dashboard",
+    },
+  ];
+  return (
+    <>
+      <SidebarInset>
+        <BreadCrumb breadcrumbList={breadcrumbList} />
+        <div className="space-y-4 lg:space-y-6">
+          <InvoiceStats />
+          <div className="grid gap-4 lg:gap-6 md:grid-cols-2">
+            <div className="rounded-md border max-h-[460px] overflow-y-auto custom-scrollbar">
+              <Overview />
+            </div>
+            <div className="border rounded-md p-4 lg:p-6 max-h-[460px] overflow-auto custom-scrollbar">
+              <RecentSales />
+            </div>
+            <div className="border rounded-md p-4 lg:p-6 max-h-[460px] overflow-auto custom-scrollbar">
+              <PaymentOverview />
+            </div>
+            <div className="border rounded-md p-4 lg:p-6 max-h-[460px] overflow-auto custom-scrollbar">
+              <RecentTransactions />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </>
+  );
+};
+
+export default DashboardHome;
