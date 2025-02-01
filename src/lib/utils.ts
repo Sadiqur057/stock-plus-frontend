f8c0c270b1b3ea78308c6c24e265e87500b759b0
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-01 21:39:32 +0600

    created function to get formatted date string

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { parse, format } from "date-fns";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getFormattedDate(date: string) {
+  const parsedDate = parse(date ?? "", "M/d/yyyy, h:mm:ss a", new Date());
+  const formattedDate = format(parsedDate, "MMMM do, yyyy 'at' hh:mm a");
+  return formattedDate;
 }
