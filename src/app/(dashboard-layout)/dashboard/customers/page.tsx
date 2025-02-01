9c1ba5cf887ff8e38327a37fad1bc68d47e71281
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-01 12:21:46 +0600

    added customer page

---
+++ src/app/(dashboard-layout)/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+
+import CustomersPage from "@/components/dashboard/Customers/CustomersPage";
+import React from "react";
+
+const page = () => {
+  return <CustomersPage />;
+};
+
+export default page;
