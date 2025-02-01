01dcbbee226f96f6acfe5bcabb3b932985a38598
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-01 12:21:59 +0600

    handled functionalities and api integration for customers

---
+++ src/components/dashboard/Customers/AddCustomer.tsx
@@ -0,0 +1,129 @@
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
+import toast from "react-hot-toast";
+import {
+  QueryObserverResult,
+  RefetchOptions,
+  useMutation,
+} from "@tanstack/react-query";
+import api from "@/interceptors/api";
+import { ProductShape } from "@/types/product.type";
+import { ClipboardPlus } from "lucide-react";
+
+type FormData = {
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+};
+
+type Props = {
+  closeModal: () => void;
+  refetch: (
+    options?: RefetchOptions
+  ) => Promise<QueryObserverResult<ProductShape[], Error>>;
+};
+
+
+const AddCustomer = ({ refetch, closeModal }: Props) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const { mutate } = useMutation({
+    mutationFn: async (data: FormData) => {
+      const result = await api.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/add-customer`,
+        data
+      );
+      console.log("checking result", result);
+      if (!result.data.success) {
+        return toast.error(result?.data?.message || "Something went wrong.");
+      }
+      toast.success(result?.data?.message);
+      refetch();
+      closeModal();
+    },
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    mutate(formData);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div className="space-y-4 lg:space-y-6">
+          <div className="space-y-1">
+            <Label htmlFor="name">Customer Name</Label>
+            <Input
+              id="name"
+              value={formData?.name}
+              name="name"
+              type="text"
+              placeholder="Enter customer name"
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="email">Customer Email</Label>
+            <Input
+              id="email"
+              name="email"
+              type="text"
+              value={formData?.email}
+              placeholder="Enter customer email"
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="phone">Customer Phone Number</Label>
+            <Input
+              id="phone"
+              name="phone"
+              type="text"
+              placeholder="Enter customer phone"
+              value={formData?.phone}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="space-y-1">
+            <Label htmlFor="address">Customer Address</Label>
+            <Input
+              id="address"
+              name="address"
+              placeholder="Enter customer address"
+              type="text"
+              value={formData?.address}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <Button type="submit" className="w-full">
+            <ClipboardPlus /> Add Customer
+          </Button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default AddCustomer;
