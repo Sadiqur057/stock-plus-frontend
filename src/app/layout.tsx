46a4208a99661e1cf6eb2ae3aef8ed8cc5908f38
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-04 11:04:44 +0600

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "@/assets/styles/global.scss";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: {
@@ -53,6 +54,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Analytics />
         <Toaster position="top-center" />
         {children}
       </body>
