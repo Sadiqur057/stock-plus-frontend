f15a9c44a2dfc6ed1fa7c141383981e6ac2b43f1
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-05 16:14:20 +0600

    added utility function to get readable date

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
-import { parse, format } from "date-fns";
+import { parse, format, parseISO } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -10,4 +10,13 @@ export function getFormattedDate(date: string) {
   const parsedDate = parse(date ?? "", "M/d/yyyy, h:mm:ss a", new Date());
   const formattedDate = format(parsedDate, "MMMM do, yyyy 'at' hh:mm a");
   return formattedDate;
+}
+
+export function getReadableDate(data: string) {
+  const formattedDate = format(parseISO(data), "h:mma, dd/MM/yyyy");
+  return formattedDate;
+}
+
+export function getFormattedPrice(price: number) {
+  return price.toLocaleString("en-IN");
 }
