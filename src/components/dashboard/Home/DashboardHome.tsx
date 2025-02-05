2bb1c1693bc7a3d7e5113c6c96a3c40cbb4b8d6e
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-05 16:11:45 +0600

    handled api integration for to get customized dashboard data

--- src/components/dashboard/Home/DashboardHome.tsx
+++ src/components/dashboard/Home/DashboardHome.tsx
@@ -1,11 +1,17 @@
+"use client";
 import React from "react";
 import { RecentSales } from "./RecentSales";
-import { Overview } from "./Overview";
+import { InvoiceOverview } from "./InvoiceOverview";
 import { SidebarInset } from "@/components/ui/sidebar";
 import BreadCrumb from "@/components/shared/dashboard/BreadCrumb";
 import { InvoiceStats } from "./InvoiceStats";
 import { PaymentOverview } from "./PaymentOverview";
 import { RecentTransactions } from "./RecentTransaction";
+import api from "@/interceptors/api";
+import toast from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import ScreenLoader from "@/components/shared/Loader/ScreenLoader";
+import { DashboardFilter } from "./DashboardFilter";
 
 const DashboardHome = () => {
   const breadcrumbList = [
@@ -14,27 +20,49 @@ const DashboardHome = () => {
       link: "/dashboard",
     },
   ];
+
+  const {
+    data: data,
+    isLoading,
+    // refetch,
+  } = useQuery({
+    queryKey: ["attributes"],
+    queryFn: async () => {
+      const result = await api.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/dashboard`
+      );
+      if (!result?.data?.success) {
+        return toast.error(result?.data?.message || "Something went wrong");
+      }
+      return result?.data?.data;
+    },
+  });
   return (
     <>
       <SidebarInset>
         <BreadCrumb breadcrumbList={breadcrumbList} />
-        <div className="space-y-4 lg:space-y-6">
-          <InvoiceStats />
-          <div className="grid gap-4 lg:gap-6 md:grid-cols-2">
-            <div className="rounded-md border max-h-[460px] overflow-y-auto custom-scrollbar">
-              <Overview />
-            </div>
-            <div className="border rounded-md p-4 lg:p-6 max-h-[460px] overflow-auto custom-scrollbar">
-              <RecentSales />
-            </div>
-            <div className="border rounded-md p-4 lg:p-6 max-h-[460px] overflow-auto custom-scrollbar">
-              <PaymentOverview />
-            </div>
-            <div className="border rounded-md p-4 lg:p-6 max-h-[460px] overflow-auto custom-scrollbar">
-              <RecentTransactions />
+        <DashboardFilter />
+        {isLoading ? (
+          <ScreenLoader />
+        ) : (
+          <div className="space-y-4 lg:space-y-6">
+            <InvoiceStats summary={data?.summary} />
+            <div className="grid gap-4 lg:gap-6 md:grid-cols-2">
+              <div className="border rounded-md p-4 lg:p-6 max-h-[472px] overflow-auto custom-scrollbar">
+                <InvoiceOverview chartData={data?.invoiceChartData}/>
+              </div>
+              <div className="border rounded-md p-4 lg:p-6 max-h-[472px] overflow-auto custom-scrollbar">
+                <RecentSales invoices={data?.invoices} />
+              </div>
+              <div className="border rounded-md p-4 lg:p-6 max-h-[472px] overflow-auto custom-scrollbar">
+                <RecentTransactions transactions={data?.transactions} />
+              </div>
+              <div className="border rounded-md p-4 lg:p-6 max-h-[472px] overflow-auto custom-scrollbar">
+                <PaymentOverview chartData={data?.paymentChartData} />
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </SidebarInset>
     </>
   );
