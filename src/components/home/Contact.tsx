0d2d74de3234537df994d0539ba5febf45fdbcac
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-06 21:23:47 +0600

    fixed spacing

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -95,7 +95,7 @@ export default function Contact() {
   };
 
   return (
-    <div id="contact" className="container mx-auto py:20 lg:py-24">
+    <div id="contact" className="container mx-auto py-20 lg:py-24">
       <div className="border mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-2">
           <div className="p-6 lg:p-8 bg-gradient-to-br from-primary/10 to-secondary/10 rounded-l-lg flex flex-col justify-center">
