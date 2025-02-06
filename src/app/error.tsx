f37fe00fc70ed74487fed7e05bb880b8e9d25d81
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-06 21:14:54 +0600

    styled error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -11,11 +11,11 @@ export default function Error() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-background">
       <div>
-        <Image src={errorImage} alt="Not found 404" width={420} height={120} />
+        <Image src={errorImage} alt="Not found 404" width={220} height={100} />
       </div>
-      <div className="w-full max-w-md">
+      <div className="w-full max-w-md mt-10">
         <CardContent className="text-center">
-          <p className="text-xl mb-4">Oops! Something unexpected happened.</p>
+          <p className="text-xl font-medium mb-4">Oops! Something unexpected happened.</p>
           <p className="text-muted-foreground mb-4">
             The content you are looking for might have some issues. Please try
             reloading the page.
