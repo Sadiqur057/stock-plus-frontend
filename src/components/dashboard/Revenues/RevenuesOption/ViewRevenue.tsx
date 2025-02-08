ddf0eed16503e6887d3a6c0ba0ea94f0dbe54803
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-08 20:51:48 +0600

    handled functionality and api integration to view revenue

---
+++ src/components/dashboard/Revenues/RevenuesOption/ViewRevenue.tsx
@@ -0,0 +1,114 @@
+import {
+  CalendarClock,
+} from "lucide-react";
+import Loader from "@/components/ui/Loader";
+import { beautifyDate, getFormattedPrice } from "@/lib/utils";
+import { RevenueType } from "../RevenuesPage";
+import { Separator } from "@/components/ui/separator";
+
+type RevenueProps = {
+  revenueData: RevenueType;
+  isLoading: boolean;
+};
+const ViewRevenue = ({ revenueData: data, isLoading }: RevenueProps) => {
+  if (isLoading) {
+    return (
+      <div className="min-h-[50vh] flex items-center justify-center">
+        <Loader />
+      </div>
+    );
+  }
+  return (
+    <>
+      <>
+        <div className="flex justify-between">
+          <p className="text-2xl font-semibold mb-4">
+            Invoice Revenue
+          </p>
+          <div className="text-right">
+            <p className="text-sm text-muted-foreground flex gap-2 items-center">
+              <CalendarClock size="16" /> {beautifyDate(data?.created_at)}
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="space-y-1">
+            <p className="text-sm font-medium">Customer</p>
+            <p className="text-sm text-muted-foreground">
+              {data?.customer_name}
+            </p>
+          </div>
+          <div>
+            <p className="text-sm font-medium">Created by</p>
+            <p className="text-sm text-muted-foreground">{data?.created_by}</p>
+          </div>
+        </div>
+
+        <div className="grid gap-6 mt-6">
+          <Separator />
+          <div className="flex justify-between gap-4 py-2">
+            <div className="space-y-1">
+              <p className="text-sm font-medium text-muted-foreground">
+                Total Cost
+              </p>
+              <p className="text-2xl font-semibold">
+                BDT. {getFormattedPrice(data?.total_cost)}
+              </p>
+            </div>
+            <div className="space-y-1">
+              <p className="text-sm font-medium text-muted-foreground">
+                Revenue
+              </p>
+              <p className="text-2xl font-semibold text-green-600">
+                BDT. {getFormattedPrice(data?.revenue)}
+              </p>
+            </div>
+            <div className="space-y-1">
+              <p className="text-sm font-medium text-muted-foreground">
+                Revenue %
+              </p>
+              <p className="text-2xl font-semibold text-blue-600">
+                {data?.revenue_percentage.toFixed(2)}%
+              </p>
+            </div>
+          </div>
+          <Separator />
+          <div>
+            <h3 className="text-lg font-semibold mb-2">Products</h3>
+            <ul className="space-y-2">
+              {data?.products?.map((product) => (
+                <li
+                  key={product._id}
+                  className="flex justify-between items-center"
+                >
+                  <div>
+                    <p className="font-medium">{product.productName}</p>
+                    <p className="text-sm text-muted-foreground">
+                      {product.company}
+                    </p>
+                  </div>
+                  <div className="text-right">
+                    <p className="font-medium">
+                      {getFormattedPrice(Number(product.salePrice.toFixed(2)))}{" "}
+                      x {product.quantity}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      Total: &nbsp;
+                      {getFormattedPrice(
+                        Number(
+                          (product.salePrice * product.quantity).toFixed(2)
+                        )
+                      ) }  BDT.
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </>
+    </>
+  );
+};
+
+export default ViewRevenue;
