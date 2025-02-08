22930b9ace43a1fab5f3d15e941a68b5416eb1f0
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-08 18:18:06 +0600

    made invoice summary dynamic

--- src/components/dashboard/Invoice/InvoiceSummary.tsx
+++ src/components/dashboard/Invoice/InvoiceSummary.tsx
@@ -1,32 +1,48 @@
+import { getFormattedPrice } from "@/lib/utils";
 import React from "react";
 
-const InvoiceSummary = () => {
-  const totalInvoices = 12;
-  const totalAmount = 1200;
-  const paidInvoices = 7;
-  const pendingInvoices = 5;
+type Props = {
+  summary: {
+    invoice_count: number;
+    total_invoice_amount: number;
+    paid_invoice_count: number;
+    due_invoice_count: number;
+  };
+};
+
+const InvoiceSummary = ({ summary }: Props) => {
+  const {
+    invoice_count,
+    total_invoice_amount,
+    paid_invoice_count,
+    due_invoice_count,
+  } = summary || {};
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6 my-4 lg:my-8">
         <div className="space-y-2 p-4 border bg-gray-50 rounded-md">
           <p className="text-sm font-medium text-gray-500">Total Invoices</p>
-          <p className="text-xl md:text-2xl font-semibold">{totalInvoices}</p>
+          <p className="text-xl md:text-2xl font-semibold">{invoice_count}</p>
         </div>
 
         <div className="space-y-2 p-4 border bg-gray-50 rounded-md">
           <p className="text-sm font-medium text-gray-500">Total Amount</p>
-          <p className="text-xl md:text-2xl font-semibold">${totalAmount.toFixed(2)}</p>
+          <p className="text-xl md:text-2xl font-semibold">
+            BDT. {getFormattedPrice(total_invoice_amount) || 0}
+          </p>
         </div>
 
         <div className="space-y-2 p-4 border bg-gray-50 rounded-md">
           <p className="text-sm font-medium text-gray-500">Paid Invoices</p>
-          <p className="text-xl md:text-2xl font-semibold text-green-600">{paidInvoices}</p>
+          <p className="text-xl md:text-2xl font-semibold text-green-600">
+            {paid_invoice_count}
+          </p>
         </div>
 
         <div className="space-y-2 p-4 border bg-gray-50 rounded-md">
           <p className="text-sm font-medium text-gray-500">Pending Invoices</p>
           <p className="text-xl md:text-2xl font-semibold text-yellow-600">
-            {pendingInvoices}
+            {due_invoice_count}
           </p>
         </div>
       </div>
