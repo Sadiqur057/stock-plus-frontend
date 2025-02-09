28dc42193df06572103f025799df332258a1ed2d
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-09 21:37:36 +0600

    styled heading section

--- src/components/dashboard/Revenues/RevenuesOption/ViewRevenue.tsx
+++ src/components/dashboard/Revenues/RevenuesOption/ViewRevenue.tsx
@@ -21,17 +21,17 @@ const ViewRevenue = ({ revenueData: data, isLoading }: RevenueProps) => {
   return (
     <>
       <>
-        <div className="flex justify-between">
-          <p className="text-2xl font-semibold mb-4">
+        <div className="flex flex-col sm:flex-row justify-between gap-2  mb-3 sm:mb-4">
+          <p className="text-xl lg:text-2xl font-semibold">
             Invoice Revenue
           </p>
-          <div className="text-right">
+          <div className="sm:text-right">
             <p className="text-sm text-muted-foreground flex gap-2 items-center">
               <CalendarClock size="16" /> {beautifyDate(data?.created_at)}
             </p>
           </div>
         </div>
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col sm:flex-row justify-between sm:items-center gap-2">
           <div className="space-y-1">
             <p className="text-sm font-medium">Customer</p>
             <p className="text-sm text-muted-foreground">
@@ -40,18 +40,18 @@ const ViewRevenue = ({ revenueData: data, isLoading }: RevenueProps) => {
           </div>
           <div>
             <p className="text-sm font-medium">Created by</p>
-            <p className="text-sm text-muted-foreground">{data?.created_by}</p>
+            <p className="text-sm text-muted-foreground">{data?.created_by_name}</p>
           </div>
         </div>
 
         <div className="grid gap-6 mt-6">
           <Separator />
-          <div className="flex justify-between gap-4 py-2">
+          <div className="flex flex-col md:flex-row justify-between gap-2 md:py-2">
             <div className="space-y-1">
               <p className="text-sm font-medium text-muted-foreground">
                 Total Cost
               </p>
-              <p className="text-2xl font-semibold">
+              <p className="text-xl md:text-2xl font-semibold">
                 BDT. {getFormattedPrice(data?.total_cost)}
               </p>
             </div>
@@ -59,7 +59,7 @@ const ViewRevenue = ({ revenueData: data, isLoading }: RevenueProps) => {
               <p className="text-sm font-medium text-muted-foreground">
                 Revenue
               </p>
-              <p className="text-2xl font-semibold text-green-600">
+              <p className="text-xl md:text-2xl font-semibold text-green-600">
                 BDT. {getFormattedPrice(data?.revenue)}
               </p>
             </div>
@@ -67,7 +67,7 @@ const ViewRevenue = ({ revenueData: data, isLoading }: RevenueProps) => {
               <p className="text-sm font-medium text-muted-foreground">
                 Revenue %
               </p>
-              <p className="text-2xl font-semibold text-blue-600">
+              <p className="text-xl md:text-2xl font-semibold text-blue-600">
                 {data?.revenue_percentage.toFixed(2)}%
               </p>
             </div>
@@ -75,11 +75,11 @@ const ViewRevenue = ({ revenueData: data, isLoading }: RevenueProps) => {
           <Separator />
           <div>
             <h3 className="text-lg font-semibold mb-2">Products</h3>
-            <ul className="space-y-2">
+            <ul className="space-y-3">
               {data?.products?.map((product) => (
                 <li
                   key={product._id}
-                  className="flex justify-between items-center"
+                  className="flex gap-2 flex-col sm:flex-row sm:justify-between sm:items-center"
                 >
                   <div>
                     <p className="font-medium">{product.productName}</p>
@@ -87,7 +87,7 @@ const ViewRevenue = ({ revenueData: data, isLoading }: RevenueProps) => {
                       {product.company}
                     </p>
                   </div>
-                  <div className="text-right">
+                  <div className="sm:text-right">
                     <p className="font-medium">
                       {getFormattedPrice(Number(product.salePrice.toFixed(2)))}{" "}
                       x {product.quantity}
