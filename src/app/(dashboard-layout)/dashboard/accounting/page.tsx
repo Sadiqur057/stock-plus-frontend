540938d65bd3be7c9cfaf4d797ce218d0075ad8a
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-10 22:11:23 +0600

    added page for accounting

---
+++ src/app/(dashboard-layout)/dashboard/accounting/page.tsx
@@ -0,0 +1,11 @@
+import AccountingPage from '@/components/dashboard/Accounting/AccountingPage';
+import React from 'react';
+
+const page = () => {
+  return (
+    <AccountingPage/>
+      
+  );
+};
+
+export default page;
