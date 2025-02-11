cbbd3060afb327d5b3a7e74d37d1c7c95658b02b
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-11 16:40:45 +0600

    showed customer and supplier information

--- src/components/dashboard/Transaction/TransactionOption/ViewTransaction.tsx
+++ src/components/dashboard/Transaction/TransactionOption/ViewTransaction.tsx
@@ -31,16 +31,26 @@ const ViewTransaction = ({ transactionData, isLoading }: TransactionProps) => {
           <div className="space-y-6">
             <div>
               <h3 className="text-sm font-medium text-muted-foreground mb-2">
-                Customer Information
+                {transactionData?.customer
+                  ? "Customer Information"
+                  : "Supplier Information"}
               </h3>
               <div className="space-y-2">
                 <div className="flex items-center gap-2">
                   <User className="h-4 w-4 text-gray-600" />
-                  <span>{transactionData?.customer?.name}</span>
+                  <span>
+                    {transactionData?.customer
+                      ? transactionData?.customer?.name
+                      : transactionData?.supplier?.name}
+                  </span>
                 </div>
                 <div className="flex items-center gap-2">
                   <Mail className="h-4 w-4 text-gray-600" />
-                  <span>{transactionData?.customer?.email}</span>
+                  <span>
+                    {transactionData?.customer
+                      ? transactionData?.customer?.email
+                      : transactionData?.supplier?.email}
+                  </span>
                 </div>
               </div>
             </div>
