6556183a7148f248571bc583fdddb64121ce65f4
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-13 22:28:55 +0600

    customized status filter reusable component to filter by status

---
+++ src/components/dashboard/Filter/StatusFilter.tsx
@@ -0,0 +1,44 @@
+import React, { Dispatch, SetStateAction } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+type TProps = {
+  selectedStatus: string;
+  setSelectedStatus: Dispatch<SetStateAction<string>>;
+};
+
+const status = [
+  { label: "All", value: "all" },
+  { label: "Paid", value: "paid" },
+  { label: "Unpaid", value: "unpaid" },
+  { label: "Partially Paid", value: "partially paid" },
+];
+
+const StatusFilter = ({
+  selectedStatus,
+  setSelectedStatus,
+}: TProps) => {
+  return (
+    <div className="xl:w-[220px]">
+      <Select value={selectedStatus} onValueChange={setSelectedStatus}>
+        <SelectTrigger className="py-[22px] min-w-[160px] xl:w-[220px]">
+          <SelectValue placeholder="Select status" />
+        </SelectTrigger>
+        <SelectContent>
+          {status?.map((s) => (
+            <SelectItem key={s.value} value={s.value}>
+              {s.label}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default StatusFilter;
